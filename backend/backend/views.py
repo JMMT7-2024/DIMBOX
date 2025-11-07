be366a82069f0ec4aa13f848bfe00be53200527a
root <root@DESKTOP-FIIEM94.localdomaincancel>
2025-11-07 00:29:34 -0500

    chore: Add Firebase credentials to gitignore and remove sensitive files

--- backend/backend/views.py
+++
@@ -1,7 +0,0 @@
-# backend/backend/views.py
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def home(request):
-    return HttpResponse("Bienvenido a la API del backend")
